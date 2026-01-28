b502ae024fa7f269d2f5f10883295aee76b1d3ff
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-01-27 20:11:40 -0600

    Medio hecho, ya solo de copiar y pegar contenido y personalizacion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,157 @@
+<nav>
+    <ul class="flex flex-row gap-10 p-4 justify-center shadow-lg w-full font-bold">
+        <li>
+            <a href="#inicio" class="p-2 hover:bg-gray-200 rounded">Inicio</a>
+        </li>
+        <li>
+            <a href="#example" class="p-2 hover:bg-gray-200 rounded">Concepto</a>
+        </li>
+        <li>
+            <a href="#objetivos" class="p-2 hover:bg-gray-200 rounded">Objetivos</a>
+        </li>
+        <li>
+            <a href="#usos" class="p-2 hover:bg-gray-200 rounded">Usos</a>
+        </li>
+        <li>
+            <a href="#arquitectura_bd" class="p-2 hover:bg-gray-200 rounded">Arquitectura de una bd</a>
+        </li>
+        <li>
+            <a href="#arquitectura_sgbd" class="p-2 hover:bg-gray-200 rounded">Arquitectura del SGBD</a>
+        </li>
+    </ul>
+</nav>
 
-<h1 class="text-center font-extrabold mt-20">HOLA MUNDO ESTE ES UN TEST</h1>
+<!--NOTAS Y INFORMCION-->
+<div class="flex gap-6 flex-row h-[530px] ">
+
+    <section class="flex-1 h-130 bg-white shadow-md mt-5 ml-6 rounded border border-gray-300 font-['Times_New_Roman'] overflow-y-auto">
+
+        <div class="flex flex-col">
+            <h1 id="inicio" class="m-3 font-extrabold text-4xl">Bases de datos</h1>
+            <p class="ml-3 mb-2 text-sm">27 de enero de 2026</p>
+            <div class="flex justify-center">
+                <img src="/imgs/ilustracion.jpg" alt="Ilustración de bases de datos" class="w-2xl h-60 rounded mx-10"/>
+            </div>
+
+            <div class="animate-pulse p-2 mt-2 mx-3">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </div>
+
+            <div class="animate-pulse p-2 mt-2 mx-3">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </div>
+
+            <div class="animate-pulse p-2 mt-2 mx-3">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </div>
+
+            <div class="animate-pulse p-2 mt-2 mx-3">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </div>
+
+            <div class="animate-pulse p-2 mt-2 mx-3">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </div>
+
+            <div class="animate-pulse p-2 mt-2 mx-3">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </div>
+
+            <div class="animate-pulse p-2 mt-2 mx-3">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </div>
+
+            <div id="example" class="animate-pulse p-2 mt-2 mx-3">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </div>
+        </div>
+    </section>
+
+     <section class="w-60 h-130 bg-white shadow-md mt-5 mr-5 rounded border border-gray-300 font-['Times_New_Roman']">
+
+        <h2 class="mt-3 ml-3 font-semibold text-lg">Universidad</h2>
+        <div class="flex flex-col items-center justify-center">
+            <img src="/imgs/ITVH.jpg" alt="Logo del instituto" class="w-20 h-20 rounded mb-2"/>
+            <a class="text-center" href="https://villahermosa.tecnm.mx/">Instituto Tecnológico de Villahermosa</a>
+        </div>
+
+        <h2 class="mt-2 ml-3 font-semibold text-lg">Autores</h2>
+        <ul>
+            <li class="m-3 text-sm">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </li>
+            <li class="m-3 text-sm">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </li>
+            <li class="m-3 text-sm">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </li>
+            <li class="m-3 text-sm">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </li>
+            <li class="m-3 text-sm">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </li>
+        </ul>
+
+        <h2 class="mt-2 ml-3 font-semibold text-lg">Correos</h2>
+        <ul>
+            <li class="m-3 text-sm">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </li>
+            <li class="m-3 text-sm">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </li>
+            <li class="m-3 text-sm">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </li>
+            <li class="m-3 text-sm">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </li>
+            <li class="m-3 text-sm">
+                <div class="h-3 bg-slate-700 rounded w-full mb-1"></div>
+            </li>
+        </ul>
+    </section>
+    
+</div>
